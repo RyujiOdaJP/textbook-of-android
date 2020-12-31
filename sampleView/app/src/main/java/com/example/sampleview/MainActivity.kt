866464210bf4d0528d7336065dfcefc23cb11015
Ryuji Oda <ryuji.oda@gmail.com>
2020-12-31 16:40:10 +0900

    wip

--- sampleView/app/src/main/java/com/example/sampleview/MainActivity.kt
+++ sampleView/app/src/main/java/com/example/sampleview/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.sampleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.btSave)
+        button.setOnClickListener {
+
+        }
     }
+
+    private inner class Hello : View.OnClickListener {
+        override fun onClick(v: View?) {
+            TODO("Not yet implemented")
+        }
+    }
+
+
 }
