a15ce6e603daeb3ff3591e6a6f7f4bbfd791c37d
Ryuji Oda <ryuji.oda@gmail.com>
2021-01-06 22:33:53 +0900

    add simple adapter

--- IntentSample/app/src/main/java/com/example/intentsample/MainActivity.kt
+++ IntentSample/app/src/main/java/com/example/intentsample/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.intentsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.SimpleAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        val menuList: MutableList<MutableMap<String, String>> = mutableListOf()
+        var menu = mutableMapOf("name" to "唐揚げ定食", "price" to "800円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ハンバーグ定食", "price" to "850円")
+        menuList.add(menu)
+
+        val from = arrayOf("name", "price")
+
+        val to = intArrayOf(android.R.id.text1, android.R.id.text2)
+
+        val adapter = SimpleAdapter(
+            applicationContext,
+            menuList,
+            android.R.layout.simple_list_item_2,
+            from,
+            to
+        )
+        lvMenu.adapter = adapter
     }
 }
