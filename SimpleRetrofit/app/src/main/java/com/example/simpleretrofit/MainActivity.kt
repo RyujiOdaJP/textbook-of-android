b308faecf8ac39617de11ccb7106be49ed90eea4
Ryuji Oda <ryuji.oda@gmail.com>
2021-01-16 17:29:14 +0900

    success to show simple api

--- SimpleRetrofit/app/src/main/java/com/example/simpleretrofit/MainActivity.kt
+++ SimpleRetrofit/app/src/main/java/com/example/simpleretrofit/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.widget.TextView
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -18,6 +19,7 @@ data class Jojo(val name: String = "", val stand: String = "")
 interface JojoService {
     @GET("jojo.json")
     fun fetchJojo(): Call<Jojo>
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,21 +27,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val retrofit = Retrofit.Builder()
-                .baseUrl("http://apppppp.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+            .baseUrl("http://apppppp.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
         val handler = Handler()
+        Log.d("sdddddf","as")
 
         thread {
             try {// Retrofitはメインスレッドで処理できない
-                val service:JojoService = retrofit.create(JojoService::class.java)
-                val jojo = service.fetchJojo().execute().body() ?: throw IllegalStateException("bodyがnullだよ！")
+                val service: JojoService = retrofit.create(JojoService::class.java)
+                val jojo = service.fetchJojo().execute().body()
+                    ?: throw IllegalStateException("bodyがnullだよ！")
+                Log.d("sdf",jojo.name)
 
-            } catch (e:Exception) {
-                Log.d("mapi", "debug $e")
+                handler.post(Runnable {
+                    val nameTextView = findViewById<TextView>(R.id.name)
+                    val standTextView = findViewById<TextView>(R.id.stand)
+
+                    Log.d("a", jojo.stand)
+                    nameTextView.text = jojo.name
+                    standTextView.text = jojo.stand
+                })
+
+            } catch (e: Exception) {
+                Log.d("mopi", "debug $e")
             }
         }
+
     }
-    }
+
 }
