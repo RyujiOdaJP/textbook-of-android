55f9e99b32db8bba0a35ca39cb162b718f21b7a7
Ryuji Oda <ryuji.oda@gmail.com>
2021-01-25 10:26:13 +0900

    init

---
+++ UdemyCourse/app/src/main/java/KotlinBasics/KotlinBasic.kt
@@ -0,0 +1,18 @@
+package KotlinBasics
+
+fun main() {
+    println("Hi")
+
+    // Type inference: Kotlin assumes automatically data type.
+    val age = 30
+
+    val myStr = "Hello world"
+    var firstChar: Char = myStr[0]
+    var lastChar: Char = myStr[myStr.length - 1]
+
+    println("last character is " +  lastChar) // d
+
+    var result = 5 + 4
+    result /= 2 // assign new value into result as result = result /2
+    print(result)
+}
