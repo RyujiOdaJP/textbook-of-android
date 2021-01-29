a0a055102599b4ef788982c40f0449067bae1de1
Ryuji Oda <ryuji.oda@gmail.com>
2021-01-29 10:10:01 +0900

    assign two paramators

--- UdemyCourse/app/src/main/java/KotlinBasics/User.kt
+++ UdemyCourse/app/src/main/java/KotlinBasics/User.kt
@@ -1,6 +1,6 @@
 package KotlinBasics
 
-data class User (val id: Long, var name: String)
+data class User(val id: Long, var name: String)
 
 fun main() {
     val user1 = User(1, "Denis")
@@ -14,11 +14,13 @@ fun main() {
     println(user2.name)
     println(user1.name == user2.name)
 
-    val updatedUser = user1.copy(name="Jack")
+    val updatedUser = user1.copy(name = "Jack")
     println(user1)
     println(updatedUser)
 
     println(updatedUser.component1())
     println(updatedUser.component2())
 
+    val (id, name) = updatedUser
+    println("id = $id, name = $name")
 }
