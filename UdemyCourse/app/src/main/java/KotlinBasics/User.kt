3b6837f1e090f440e301e5f2fe58a331f0a96b73
Ryuji Oda <ryuji.oda@gmail.com>
2021-01-29 10:07:06 +0900

    basic data class

---
+++ UdemyCourse/app/src/main/java/KotlinBasics/User.kt
@@ -0,0 +1,24 @@
+package KotlinBasics
+
+data class User (val id: Long, var name: String)
+
+fun main() {
+    val user1 = User(1, "Denis")
+
+    println(user1.name)
+    user1.name = "Michael"
+    println(user1.name)
+
+    val user2 = User(1, "Michael")
+    println(user1.name)
+    println(user2.name)
+    println(user1.name == user2.name)
+
+    val updatedUser = user1.copy(name="Jack")
+    println(user1)
+    println(updatedUser)
+
+    println(updatedUser.component1())
+    println(updatedUser.component2())
+
+}
