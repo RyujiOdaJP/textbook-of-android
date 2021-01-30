c3ef73a78f1fcc717a170f67748a0101f0db8941
Ryuji Oda <ryuji.oda@gmail.com>
2021-01-30 23:17:40 +0900

    interface

---
+++ UdemyCourse/app/src/main/java/KotlinBasics/Drivable.kt
@@ -0,0 +1,9 @@
+package KotlinBasics
+
+interface Drivable {
+    val maxSpeed: Double
+    fun drive(): String
+    fun brake(){
+        println("The drivable is braking")
+    }
+}
--- UdemyCourse/app/src/main/java/KotlinBasics/InheritanceCar.kt
+++ UdemyCourse/app/src/main/java/KotlinBasics/InheritanceCar.kt
@@ -1,7 +1,11 @@
 package KotlinBasics
 
 // super class
-open class ParentCar(val name: String, val brand: String) {
+open class ParentCar(
+    override val maxSpeed: Double,
+    val name: String,
+    val brand: String
+) : Drivable {
     // "open" makes inheritable because in default, class would be final by default
     open var range: Double = 0.0
 
@@ -14,21 +18,40 @@ open class ParentCar(val name: String, val brand: String) {
     open fun drive(distance: Double) {
         println("Drove for $distance km")
     }
+
+    override fun drive(): String {
+        return "driving the interface"
+    }
 }
 
 // sub class
-class ElectricCar(name: String, brand: String, batteryLife: Double = 0.0) : ParentCar(name, brand) {
+class ElectricCar(
+    maxSpeed: Double,
+    name: String,
+    brand: String, batteryLife: Double = 0.0
+) : ParentCar(maxSpeed, name, brand) {
+
     override var range = batteryLife * 6
+
     override fun drive(distance: Double) {
         println("Drove for $distance KM on electricity")
+    }
+
+    override fun drive(): String {
+        return "Drove for $range KM on electricity"
+    }
+
+    override fun brake() {
+        super.brake() //to call function of the super class
     }
 }
 
 fun main() {
-    var myCar = ParentCar("A3", "Audi")
-    var myECar = ElectricCar("S-Model", "Tesla", 85.0)
+    var myCar = ParentCar(200.0, "A3", "Audi")
+    var myECar = ElectricCar(200.0, "S-Model", "Tesla", 85.0)
     myECar.extendRange(200.0)
     myCar.drive(200.0)
     myECar.drive(myECar.range)
+    println(myECar.drive())
 
 }
