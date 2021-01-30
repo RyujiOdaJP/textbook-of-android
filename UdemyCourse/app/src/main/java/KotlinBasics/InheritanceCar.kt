5cf9776b2dbf8cf34080dd5e9374ab6d3ff60f7c
Ryuji Oda <ryuji.oda@gmail.com>
2021-01-30 21:21:46 +0900

    override

--- UdemyCourse/app/src/main/java/KotlinBasics/InheritanceCar.kt
+++ UdemyCourse/app/src/main/java/KotlinBasics/InheritanceCar.kt
@@ -2,12 +2,33 @@ package KotlinBasics
 
 // super class
 open class ParentCar(val name: String, val brand: String) {
-    // "open" makes inheritable because in default, class would be final
+    // "open" makes inheritable because in default, class would be final by default
+    open var range: Double = 0.0
 
+    fun extendRange(amount: Double) {
+        if (amount > 0) {
+            range += amount
+        }
+    }
+
+    open fun drive(distance: Double) {
+        println("Drove for $distance km")
+    }
 }
 
 // sub class
-class ElectricCar(name: String, brand: String, batteryLife: Double = 0.0)
-    : ParentCar(name, brand) {
+class ElectricCar(name: String, brand: String, batteryLife: Double = 0.0) : ParentCar(name, brand) {
+    override var range = batteryLife * 6
+    override fun drive(distance: Double) {
+        println("Drove for $distance KM on electricity")
+    }
+}
+
+fun main() {
+    var myCar = ParentCar("A3", "Audi")
+    var myECar = ElectricCar("S-Model", "Tesla", 85.0)
+    myECar.extendRange(200.0)
+    myCar.drive(200.0)
+    myECar.drive(myECar.range)
 
 }
