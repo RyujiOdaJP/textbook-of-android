f10ae302a3fc5f56dd07b9d89f2a513c4518b704
Ryuji Oda <ryuji.oda@gmail.com>
2021-01-30 23:57:06 +0900

    abstract class

--- UdemyCourse/app/src/main/java/KotlinBasics/Drivable.kt
+++ UdemyCourse/app/src/main/java/KotlinBasics/Drivable.kt
@@ -6,4 +6,11 @@ interface Drivable {
     fun brake(){
         println("The drivable is braking")
     }
+
+    /*
+    * Differences between interface and abstract class
+    * 1. Abstract class can be inherited by sub classes, Interface can be multiple implemented by only one class
+    * 2. Abstract class can have constructors but interface not.
+    * 3. Interface cannot hold state as field (member variables)
+    *  */
 }
