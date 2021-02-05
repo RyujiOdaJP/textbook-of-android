0f07eb0a0d3ae77d99e148650d02297d1c14ecb2
Ryuji Oda <ryuji.oda@gmail.com>
2021-02-05 09:04:45 +0900

    show recyclerView sample

--- SimpleRecyclerView/app/src/main/java/com/example/simplerecyclerview/MainActivity.kt
+++ SimpleRecyclerView/app/src/main/java/com/example/simplerecyclerview/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RecyclerViewHolder.ItemClickListener {
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var viewAdapter: RecyclerView.Adapter<*>
+    lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val hoges = resources.getStringArray(R.array.hoges).toMutableList()
+
+        viewAdapter = RecyclerAdapter(this, this, hoges)
+        viewManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+        }
+    }
+
+    override fun onItemClick(itemView: View, position: Int) {
+        Toast.makeText(applicationContext, "position $position was tapped", Toast.LENGTH_SHORT).show()
     }
 }
