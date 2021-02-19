81220b1c4392eba020359df630b2dcce935b47ec
Ryuji Oda <ryuji.oda@gmail.com>
2021-02-19 09:58:46 +0900

    add location provider

--- WeatherAPI/app/src/main/java/com/example/weatherapi/MainActivity.kt
+++ WeatherAPI/app/src/main/java/com/example/weatherapi/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.weatherapi
 
+import android.content.Context
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!isLocationEnabled()) {
+            Toast.makeText(
+                this,
+                "Please turn on your location provider",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
     }
 }
