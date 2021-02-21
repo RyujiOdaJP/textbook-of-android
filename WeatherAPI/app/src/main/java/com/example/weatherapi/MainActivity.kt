cc52de05542fef254c1dbe1be364b607221ad181
ryujioda <ryuji.oda@gmail.com>
2021-02-21 10:41:48 +0900

    set dialog button

--- WeatherAPI/app/src/main/java/com/example/weatherapi/MainActivity.kt
+++ WeatherAPI/app/src/main/java/com/example/weatherapi/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.weatherapi
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.location.LocationManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -36,7 +39,7 @@ class MainActivity : AppCompatActivity() {
                     android.Manifest.permission.ACCESS_FINE_LOCATION,
                     android.Manifest.permission.ACCESS_COARSE_LOCATION
                 )
-                .withListener(object: MultiplePermissionsListener {
+                .withListener(object : MultiplePermissionsListener {
                     override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
                         if (report?.areAllPermissionsGranted() == true) {
                             // TODO add requestLocationData
@@ -61,6 +64,26 @@ class MainActivity : AppCompatActivity() {
                 }).onSameThread()
                 .check()
         }
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It looks like you have turned off permissions")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
     private fun isLocationEnabled(): Boolean {
